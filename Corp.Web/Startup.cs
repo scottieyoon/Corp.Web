854dbacfdad64a047d1f22afec5481a3eb5283b9
Scott Yoon <scottieyoon@yahoo.com>
2018-08-02 23:11:17 -0500

    Comming Sample MVC Project with Tests project

---
+++ Corp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Corp.Web.Startup))]
+namespace Corp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
